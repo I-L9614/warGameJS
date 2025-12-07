d1ccc46c4366f0e062de9371bde1cb4ac82086f9
I-L9614 <leviitamar034@gmail.com>
2025-12-07 13:26:56 +0200

    added comprtCards function'

--- utils/deck.js
+++ utils/deck.js
@@ -1,12 +1,12 @@
-function createCard(rank,suite) {
-    const specielRank ={
-        A:14,
-        K:13,
-        Q:12,
-        J:11
+export function createCard(rank, suite) {
+    const specielRank = {
+        A: 14,
+        K: 13,
+        Q: 12,
+        J: 11
     }
     let value = +rank;
-    if(Number.isNaN(value)) {
+    if (Number.isNaN(value)) {
         value = specielRank[rank]
     }
 
@@ -17,22 +17,32 @@ function createCard(rank,suite) {
     }
 }
 
-function compareCards(p1Card,p2Card) {
-   if(p1Card.value > p2Card.value) {
-    return p1
-   } 
-   if(p2Card.value > p1Card.value) {
-    return 'p2'
-   }
-   else {
-    return 'WAR'
-   }
+export function compareCards(p1Card, p2Card) {
+    if (p1Card.value > p2Card.value) {
+        return p1
+    }
+    if (p2Card.value > p1Card.value) {
+        return 'p2'
+    }
+    else {
+        return 'WAR'
+    }
 }
+const RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+const SUITS = ['H', 'C', 'D', 'S']
 
-function createDeck() {
-
+export function createDeck() {
+    let desk = []
+    for (let i = 0; i < RANKS.length; i++) {
+        for (let j = 0; j < SUITS.length; j++) {
+            let card = createCard(RANKS[i], SUITS[j])
+            desk.push(card)
+        }
+    }
+    return desk
 }
 
 function shuffle(deck) {
 
 }
+console.log(createDeck().length)
